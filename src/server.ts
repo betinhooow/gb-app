de7a1b639c4b3ca5cd2ba39e280fd0ed918a37aa
betinhoow <robertonobre90@gmail.com>
2020-04-17 19:49:21 -0300

    Importando arquivos TS e rotas

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({
-  message: 'Hello world',
-}));
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('😆 started');
