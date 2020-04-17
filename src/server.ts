a58d644c1246252a0dcd5a2c4d2366ed04f59a15
betinhoow <robertonobre90@gmail.com>
2020-04-17 19:40:17 -0300

    Eslint, editor config

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({
-    message: 'Hello world'
-  })
-})
+app.get('/', (request, response) => response.json({
+  message: 'Hello world',
+}));
 
 app.listen(3333, () => {
-  console.log('😆 started')
-})
+  console.log('😆 started');
+});
