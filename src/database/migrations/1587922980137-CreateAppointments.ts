fc3114b3cfbed986ca6bd34ac7d56001c1fa84f1
betinhoow <robertonobre90@gmail.com>
2020-04-26 16:05:42 -0300

    Service e repository typeorm

--- src/database/migrations/1587922980137-CreateAppointments.ts
+++ src/database/migrations/1587922980137-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1587922980137
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
