0f759ff5c22e1dd34dc2f4fdc6485b8e41430672
betinhoow <robertonobre90@gmail.com>
2020-04-28 17:27:51 -0300

    Arquivos staticos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
-import { Router } from 'express';
+import express, { Router } from 'express';
 import appointmentsRouter from './appointments.route';
 import usersRouter from './users.route';
 import sessionsRouter from './sessions.route';
+import uploadConfig from '../config/upload';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/files', express.static(uploadConfig.directory));
 
 export default routes;
