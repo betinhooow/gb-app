6aa82df81facaf8c235e556b826c6d68ef9acb46
betinhoow <robertonobre90@gmail.com>
2020-04-28 18:16:42 -0300

    Lidando com erros

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,12 @@
-import express, { Router } from 'express';
+import { Router } from 'express';
 import appointmentsRouter from './appointments.route';
 import usersRouter from './users.route';
 import sessionsRouter from './sessions.route';
-import uploadConfig from '../config/upload';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
-routes.use('/files', express.static(uploadConfig.directory));
 
 export default routes;
