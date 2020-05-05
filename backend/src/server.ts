c2807515a4ef35497a38588a067ad21f1060a735
betinhoow <robertonobre90@gmail.com>
2020-05-05 15:10:46 -0300

    habilitando cors na aplicação

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 
@@ -8,6 +9,7 @@ import uploadConfig from './config/upload';
 import AppError from './error/AppError';
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 routes.use('/files', express.static(uploadConfig.directory));
