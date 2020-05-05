233968f7ea076be00c0c5c0ed0ad46557394a792
betinhoow <robertonobre90@gmail.com>
2020-05-05 17:22:45 -0300

    Logout da aplicação

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -8,7 +8,7 @@ import logoImg from '../../assets/icons/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import getValidationErrors from '../../utils/getvalidationErrors';
-import { useAuth } from '../../context/authContext';
+import { useAuth } from '../../hooks/authContext';
 
 interface FormDataCredentials {
   email: string;
