edb6703b6deb81130605f426e7de92110b47df5e
betinhoow <robertonobre90@gmail.com>
2020-05-06 20:10:18 -0300

    Animação entre páginas

--- frontend/src/pages/SignIn/styles.ts
+++ frontend/src/pages/SignIn/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 import signInBackgroundImage from '../../assets/images/sign-in-background.png';
 
@@ -8,6 +8,17 @@ export const Container = styled.div`
   align-items: stretch;
 `;
 
+const appearFromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
@@ -15,6 +26,8 @@ export const Content = styled.div`
   place-content: center;
   width: 100%;
   max-width: 700px;
+
+  animation: ${appearFromLeft} 1s;
 
   form {
     margin: 80px 0;
