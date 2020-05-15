c475aab705b122211867744bdfdfd487c6355783
Roberto Nobre <robertonobre90@gmail.com>
2020-05-15 19:07:37 -0300

    Configurando imports

--- backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,8 +1,8 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
-import AppointmentsRepository from '../repositories/AppointmentsRepository';
-import Appointment from '../entities/Appointment';
-import AppError from '../../../shared/error/AppError';
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import AppError from '@shared/error/AppError';
+import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository';
 
 interface Request {
   provider_id: string;
