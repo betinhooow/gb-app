a2ef011f476464005b7c02e0e72b95fdc6526b04
betinhoow <robertonobre90@gmail.com>
2020-05-23 20:36:18 -0300

    Testes de agendamento

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import AppError from '@shared/error/AppError';
 import CreateAppointmentService from './CreateAppointmentService';
 import MockAppointmentsRepository from '../repositories/mocks/MockAppointmentsRepository';
 
@@ -17,5 +18,27 @@ describe('CreateAppointment', () => {
 
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe('1');
+  });
+
+  it('should not be able to create two appointments in same time', async () => {
+    const mockAppointmentsRepository = new MockAppointmentsRepository();
+
+    const createAppointmentService = new CreateAppointmentService(
+      mockAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 30, 11);
+
+    await createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: '1',
+    });
+
+    expect(
+      createAppointmentService.execute({
+        date: appointmentDate,
+        provider_id: '2',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
