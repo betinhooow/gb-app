48e71429c3b978a2e152fd86c333d8f58e45be71
betinhoow <robertonobre90@gmail.com>
2020-05-29 18:37:22 -0300

    Listando dias disponiveis

--- backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -16,17 +16,57 @@ describe('LisrProviderMonthAvailability', () => {
   it('should be able to list the month availability from provider', async () => {
     await mockAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 3, 20, 8, 0, 8),
-    });
-
-    await mockAppointmentsRepository.create({
-      provider_id: 'user',
       date: new Date(2020, 4, 20, 8, 0, 8),
     });
 
     await mockAppointmentsRepository.create({
       provider_id: 'user',
+      date: new Date(2020, 4, 20, 9, 0, 8),
+    });
+
+    await mockAppointmentsRepository.create({
+      provider_id: 'user',
       date: new Date(2020, 4, 20, 10, 0, 8),
+    });
+
+    await mockAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 11, 0, 8),
+    });
+
+    await mockAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 12, 0, 8),
+    });
+
+    await mockAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 13, 0, 8),
+    });
+
+    await mockAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 14, 0, 8),
+    });
+
+    await mockAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 15, 0, 8),
+    });
+
+    await mockAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 16, 0, 8),
+    });
+
+    await mockAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 17, 0, 8),
+    });
+
+    await mockAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 21, 10, 0, 8),
     });
 
     await mockAppointmentsRepository.create({
@@ -44,7 +84,7 @@ describe('LisrProviderMonthAvailability', () => {
       expect.arrayContaining([
         { day: 19, available: true },
         { day: 20, available: false },
-        { day: 21, available: false },
+        { day: 21, available: true },
         { day: 22, available: true },
       ]),
     );
