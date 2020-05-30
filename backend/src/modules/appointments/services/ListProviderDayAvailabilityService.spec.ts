585bc9b54b3c1d2632b8b85eeecc20f297ed8e46
betinhoow <robertonobre90@gmail.com>
2020-05-29 21:33:13 -0300

    Excluindo horarios antigos

--- backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -16,12 +16,16 @@ describe('LisrProviderDayAvailability', () => {
   it('should be able to list the day availability from provider', async () => {
     await mockAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 4, 20, 8, 0, 8),
+      date: new Date(2020, 4, 20, 14, 0, 8),
     });
 
     await mockAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 4, 20, 10, 0, 8),
+      date: new Date(2020, 4, 20, 15, 0, 8),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 20, 11).getTime();
     });
 
     const availability = await listProviderDayAvailability.execute({
@@ -34,9 +38,12 @@ describe('LisrProviderDayAvailability', () => {
     expect(availability).toEqual(
       expect.arrayContaining([
         { hour: 8, available: false },
+        { hour: 9, available: false },
         { hour: 10, available: false },
-        { hour: 11, available: true },
-        { hour: 12, available: true },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
       ]),
     );
   });
