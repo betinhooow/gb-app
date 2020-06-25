04451fadb4ac49325646df2660beb8ddb844f0e3
Roberto Nobre <robertonobre90@gmail.com>
2020-06-25 16:43:31 -0300

    Listagem de agendamentos

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiClock } from 'react-icons/fi';
 import logoImg from '../../assets/icons/logo.svg';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment
+} from './styles';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
@@ -14,16 +23,45 @@ const Dashboard: React.FC = () => {
           <img src={logoImg} alt="Agengss" />
 
           <Profile>
-            <img src={user.avatar_url} alt={user.name} />
-            <div>Bem vindo,</div>
-            <strong>{user.name}</strong>
-
-            <button type="button" onClick={signOut}>
-              <FiPower />
-            </button>
+            <img src={user.avatar_url} alt="u" />
+            <div>
+              <span>Bem vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
           </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://cdn.discordapp.com/avatars/560843572358676483/233f6b8b8a689b5088682f3fb1b8a2fe?size=512"
+                alt="name"
+              />
+              <strong>Roberto Nobre</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -3,16 +3,16 @@ import { Switch } from 'react-router-dom';
 
 import SignUp from '../pages/SignUp';
 import SignIn from '../pages/SignIn';
-import Home from '../pages/Home';
 import Route from './Route';
 import ForgotPassword from '../pages/ForgotPassword';
 import ResetPassword from '../pages/ResetPassword';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
-    <Route path="/home" component={Home} isPrivate />
+    <Route path="/home" component={Dashboard} isPrivate />
     <Route path="/forgot-password" component={ForgotPassword} />
     <Route path="/reset-password" component={ResetPassword} />
   </Switch>
