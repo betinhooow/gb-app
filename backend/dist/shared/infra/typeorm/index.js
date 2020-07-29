3cafd9fdba36b7eb69ac414098746e2ecded2524
Roberto Nobre <robertonobre90@gmail.com>
2020-07-28 21:16:35 -0300

    build ormconfig

--- backend/dist/shared/infra/typeorm/index.js
+++ backend/dist/shared/infra/typeorm/index.js
@@ -2,4 +2,25 @@
 
 var _typeorm = require("typeorm");
 
-(0, _typeorm.createConnections)();
+(0, _typeorm.createConnections)([{
+  "name": "default",
+  "type": "postgres",
+  "host": "localhost",
+  "port": 5432,
+  "username": "postgres",
+  "password": "docker",
+  "database": "postgres",
+  "entities": [__dirname + "./../../../../dist/modules/**/infra/typeorm/entities/*.js"],
+  "migrations": [__dirname + "./../../../../dist/shared/infra/typeorm/migrations/*.js"],
+  "cli": {
+    "migrationsDir": __dirname + "./../../../../dist/shared/infra/typeorm/migrations"
+  }
+}, {
+  "name": "mongo",
+  "type": "mongodb",
+  "host": "localhost",
+  "port": 27017,
+  "database": "gobarber",
+  "useUnifiedTopology": true,
+  "entities": [__dirname + "./../../../../dist/modules/**/infra/typeorm/schemas/*.js"]
+}]);
--- backend/src/shared/infra/typeorm/index.ts
+++ backend/src/shared/infra/typeorm/index.ts
@@ -1,3 +1,27 @@
 import { createConnections } from 'typeorm';
 
-createConnections();
+createConnections([
+  {
+    "name": "default",
+    "type": "postgres",
+    "host": "localhost",
+    "port": 5432,
+    "username": "postgres",
+    "password": "docker",
+    "database": "postgres",
+    "entities": [__dirname + "./../../../../dist/modules/**/infra/typeorm/entities/*.js"],
+    "migrations": [__dirname + "./../../../../dist/shared/infra/typeorm/migrations/*.js"],
+    "cli": {
+      "migrationsDir": __dirname + "./../../../../dist/shared/infra/typeorm/migrations"
+    }
+  },
+  {
+    "name": "mongo",
+    "type": "mongodb",
+    "host": "localhost",
+    "port": 27017,
+    "database": "gobarber",
+    "useUnifiedTopology": true,
+    "entities": [__dirname + "./../../../../dist/modules/**/infra/typeorm/schemas/*.js"]
+  }
+]);
